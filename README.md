a41cf56e32fddd43077124afe6e244b390fbf102
Emma-Tubi <emmatubi01@gmail.com>
2024-10-11 08:19:04 -0700

    updated read me

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Build-and-Deploy-Ecommerce-Website-With-HTML-CSS-JavaScript
-[<img alt="Build-and-Deploy-Ecommerce-Website-With-HTML-CSS-JavaScript" width="100%" src="../Ecommerce/img/emmaCommerce.png" />]()
 
+[<img alt="Build-and-Deploy-Ecommerce-Website-With-HTML-CSS-JavaScript" width="100%" src="https://firebasestorage.googleapis.com/v0/b/emmacommerce-69f86.appspot.com/o/emmaCommerce.png?alt=media&token=f9b2be8b-cf71-4984-af3e-684cc1f5e713" />](https://firebasestorage.googleapis.com/v0/b/emmacommerce-69f86.appspot.com/o/emmaCommerce.png?alt=media&token=f9b2be8b-cf71-4984-af3e-684cc1f5e713)
 ## About this Project
 
 LEARNED HOW TO BUILD AND DEPLOY FULL RESPONSIVE ECOMMERCE WEBSITE USING HTML CSS & JAVASCRIPT. This is an HTML CSS project. And in this Project i learn how to build and deploy a full multipage ecommerce website completely from scratch step by step. Created from responsive navbar using html CSS JavaScript to responsive footer in one video.
--- script/script.js
+++ script/script.js
@@ -1,1 +1,28 @@
 // Script for navigation bar
+
+
+
+
+
+
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBWspVsgyau1842vBd5u-sYh6H6pc0QNPY",
+//   authDomain: "emmacommerce-69f86.firebaseapp.com",
+//   projectId: "emmacommerce-69f86",
+//   storageBucket: "emmacommerce-69f86.appspot.com",
+//   messagingSenderId: "1094447621089",
+//   appId: "1:1094447621089:web:3dde4f40f0f318e104a8a0",
+//   measurementId: "G-0RV54SSKNT"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
